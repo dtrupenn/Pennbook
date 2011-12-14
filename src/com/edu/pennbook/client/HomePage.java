0ca632e1eb96df8469dec4e911308cb387b24162
Amalia Hawkins <hawka@seas466.wireless-pennnet.upenn.edu>
2011-12-14 14:58:32 -0500

    ...

--- src/com/edu/pennbook/client/HomePage.java
+++ src/com/edu/pennbook/client/HomePage.java
@@ -6,6 +6,7 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.Cookies;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.*;
 
@@ -285,6 +286,14 @@ public class HomePage extends Composite {
 		});
 		
 		homepageMainPanel.add(postsPanel);
+		
+		Timer t = new Timer() {
+			 public void run() {
+		    	  ContentPanel.replaceContent(new HomePage(profileService));
+			 }
+		};
+
+		t.schedule(30000);
 				
 		initWidget(homepageMainPanel);
 	}
